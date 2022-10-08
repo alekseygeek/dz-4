0dae9445912966de6e49cac7584ce816e4e1cc8d
Abashin Aleksey <mmilasomm@gmail.com>
2022-10-08 22:11:50 +0300

    Решена 50 задача

--- task475052/Program.cs
+++ task475052/Program.cs
@@ -9,10 +9,10 @@
 
     double[,] numbers = new double[rows, colums];
 
-    FillArrayRandomNumbers(numbers);
+    FillArrayNumbers(numbers);
     PrintArray(numbers);
 
-    void FillArrayRandomNumbers(double[,] array)
+    void FillArrayNumbers(double[,] array)
     {
         for (int i = 0; i < rows; i++)
         {
@@ -36,14 +36,57 @@
 }
 void task50()
 {
+    //  Задача 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, 
+    // и возвращает значение этого элемента или же указание, что такого элемента нет.
+    Console.Clear();
+    Console.Write("введите номер строки до  3 :");
+    int line = Convert.ToInt32(Console.ReadLine());
+    Console.Write("введите номер столбца до 4 :");
+    int colum = Convert.ToInt32(Console.ReadLine());
+    Console.Clear();
+    int[,] numbers = new int[3, 4];
 
+    FillArrayNumbers(numbers);
+    PrintArray(numbers);
 
-
-
+    if (line > numbers.GetLength(0) || colum > numbers.GetLength(1))
+    {
+        Console.WriteLine("такого элемента нет");
+    }
+    else
+    {
+        Console.WriteLine($"значение элемента {line} строки и {colum} столбца равно {numbers[line - 1, colum - 1]}");
+    }
+    void FillArrayNumbers(int[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                array[i, j] = new Random().Next(-100, 100) / 10;
+            }
+        }
+    }
+    void PrintArray(int[,] array)
+    {
+        for (int i = 0; i < array.GetLength(0); i++)
+        {
+            Console.Write("[ ");
+            for (int j = 0; j < array.GetLength(1); j++)
+            {
+                Console.Write(array[i, j] + " ");
+            }
+            Console.Write("]");
+            Console.WriteLine("");
+        }
+    }
 
 }
 void task52()
 {
+    // Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+
+
 
 
 
